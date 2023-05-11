e5b1e397bb11dac7df8263926702d17f497e758a
Andy Assareh <assareh@me.com>
2023-05-11 14:50:19 -0700

    first

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
+variable "tfc_organization" {
+
+}
+
+variable "tfc_subnet_workspace" {
+
+}
+
 terraform {
   required_version = ">= 0.13"
+}
+
+data "tfe_outputs" "subnet" {
+  organization = var.tfc_organization
+  workspace    = var.tfc_subnet_workspace
 }
 
 provider "azurerm" {
@@ -15,40 +28,39 @@ locals {
   }
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West US 2"
+data "azurerm_resource_group" "example" {
+  name = data.tfe_outputs.subnet.rg_name
 }
 
-module "windowsservers" {
-  source  = "Azure/compute/azurerm"
-  version = "3.7.0"
+# module "windowsservers" {
+#   source  = "Azure/compute/azurerm"
+#   version = "3.7.0"
 
-  resource_group_name = azurerm_resource_group.example.name
-  is_windows_image    = true
-  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-  vm_size             = "Standard_DS1_V2"
-  admin_password      = "ComplxP@ssw0rd!"
-  vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
+#   resource_group_name = azurerm_resource_group.example.name
+#   is_windows_image    = true
+#   vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
+#   vm_size             = "Standard_DS1_V2"
+#   admin_password      = "ComplxP@ssw0rd!"
+#   vm_os_simple        = "WindowsServer"
+#   public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
+#   vnet_subnet_id      = module.network.vnet_subnets[0]
 
-  tags = local.common_tags
+#   tags = local.common_tags
 
-  depends_on = [azurerm_resource_group.example]
-}
+#   depends_on = [azurerm_resource_group.example]
+# }
 
-module "network" {
-  source  = "Azure/network/azurerm"
-  version = "3.2.1"
+# module "network" {
+#   source  = "Azure/network/azurerm"
+#   version = "3.2.1"
 
-  resource_group_name = azurerm_resource_group.example.name
-  subnet_prefixes     = ["10.0.1.0/24"]
-  subnet_names        = ["subnet1"]
+#   resource_group_name = azurerm_resource_group.example.name
+#   subnet_prefixes     = ["10.0.1.0/24"]
+#   subnet_names        = ["subnet1"]
 
-  depends_on = [azurerm_resource_group.example]
-}
+#   depends_on = [azurerm_resource_group.example]
+# }
 
-output "windows_vm_public_name" {
-  value = module.windowsservers.public_ip_dns_name
-}
+# output "windows_vm_public_name" {
+#   value = module.windowsservers.public_ip_dns_name
+# }

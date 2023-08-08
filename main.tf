ad073052e1be71611dd7cc92024c761b9c25c7e4
Andy Assareh <assareh@me.com>
2023-08-08 09:36:20 -0700

    adding function

--- main.tf
+++ main.tf
@@ -66,17 +66,53 @@ resource "random_password" "password" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
-data "azurerm_virtual_machine" "example" {
-  name                = module.linuxservers.vm_names["linux"][0]
+resource "azurerm_storage_account" "example" {
+  name                     = "${var.prefix}storageaccount"
   resource_group_name = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
- 
+
+resource "azurerm_application_insights" "example" {
+  name                = "${var.prefix}-appinsights"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  application_type    = "web"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "${var.prefix}-sp"
+  location                 = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "${var.prefix}-LFA"
+  location                 = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  service_plan_id     = azurerm_service_plan.example.id
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  site_config {
+    application_insights_connection_string = azurerm_application_insights.example.connection_string
+  }
+}
+
+data "azurerm_linux_function_app" "example" {
+  name                = azurerm_linux_function_app.example.name
+  resource_group_name = azurerm_linux_function_app.example.resource_group_name
+}
+
 check "check_vm_state" {
   assert {
-    condition = data.azurerm_virtual_machine.example.power_state == "running"
-    error_message = format("Virtual Machine (%s) should be in a 'running' status, instead state is '%s'",
-      data.azurerm_virtual_machine.example.id,
-      data.azurerm_virtual_machine.example.power_state
+    condition = data.azurerm_linux_function_app.example.usage == "Exceeded"
+    error_message = format("Function App (%s) usage has been exceeded!",
+      data.azurerm_linux_function_app.example.id,
     )
   }
 }
